1df8b5295336525e71a4e3a3e1c96e0b5dd7b7ca
Boris Bera <bera.boris@gmail.com>
2022-07-23 05:43:35 -0400

    Move chatter to own folder as it's a full component

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Chatter } from "./chatter";
+import { Chatter } from "../chatter";
 import * as classes from './header.module.scss'
 
 export function Header(): ReactElement {
