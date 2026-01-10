a4cfd9aef3cdd7b5908048fa56a31b78890bbdaf
Boris Bera <beraboris@gmail.com>
2026-01-10 12:17:38 -0500

    add colored badges to project cards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./Card";
 import { Button } from "./Button";
+import { Badge } from "./Badge";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../classnames";
 
 export interface ProjectCardProps {
   title: string;
@@ -10,16 +13,35 @@ export interface ProjectCardProps {
   githubSlug: string;
 }
 
+const techVariants = cva("", {
+  variants: {
+    tech: {
+      rust: "text-orange-600",
+      go: "text-cyan-600",
+      nix: "text-violet-600",
+      typescript: "text-sky-600",
+    },
+  },
+});
+
 export function ProjectCard({
   title,
   description,
   githubOwner,
   githubSlug,
-}: ProjectCardProps): React.JSX.Element {
+  tech,
+}: ProjectCardProps & VariantProps<typeof techVariants>) {
   return (
     <Card className="flex flex-col">
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <CardTitle className="flex justify-between">
+          {title}
+          {tech && (
+            <Badge className={cn(techVariants({ tech }))} variant="outline">
+              {tech}
+            </Badge>
+          )}
+        </CardTitle>
       </CardHeader>
       <CardContent className="grow">{description}</CardContent>
       <CardFooter>
