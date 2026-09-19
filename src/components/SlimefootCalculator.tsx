043ec213bab47be4b46d84e7e62236b43ae89212
Boris Bera <boris@dotboris.io>
2026-09-19 17:05:18 -0400

    first iteration on slimefoot simulation

--- src/components/SlimefootCalculator.tsx
+++ src/components/SlimefootCalculator.tsx
@@ -16,11 +16,11 @@ export function SlimefootCalculator() {
       const created = Math.floor(mana / 4);
       floatingMana = mana % 4;
       trace.push({
-        count,
-        triggers,
-        totalMana: mana,
-        created,
-        floatingMana,
+        Round: rounds + 1,
+        "Sacrificed Sapprolings": count,
+        "Available Mana": mana,
+        "Summoned Sapprolings": created,
+        "Floating Mana Leftover": floatingMana,
       });
       count = created;
       rounds += 1;
@@ -44,7 +44,32 @@ export function SlimefootCalculator() {
           onChange={(e) => setInitialCount(parseInt(e.target.value, 10))}
         />
       </label>
-      <pre>{JSON.stringify(res, null, 2)}</pre>
+      <dl>
+        <dt>Total Rounds</dt>
+        <dd>{res.rounds}</dd>
+        <dt>Slimefoot Triggers</dt>
+        <dd>{res.triggers}</dd>
+        <dt>Floating Leftover Mana</dt>
+        <dd>{res.floatingMana}</dd>
+      </dl>
+      <table>
+        <thead>
+          <tr>
+            {Object.keys(res.trace[0]).map((key) => (
+              <td key={key}>{key}</td>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {res.trace.map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map((value, index) => (
+                <td key={index}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
